2a97a8370bbe6adb82e7fc3b4a822e46db6c8d79
Guangyuan <254202042@qq.com>
2023-06-02 11:30:23 +0800

    feat:学习错处理中间件

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// middleware
+// 1.app level middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -32,7 +34,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  // console.log("locals = ",res.locals)
   // render the error page
   res.status(err.status || 500);
   res.render('error');
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  // throw new Error('BROKEN'); // Express will catch this on its own.
   res.send('respond with a resource');
 });
 
