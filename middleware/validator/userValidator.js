a91ae697ae79e28f8475ceb984fc860190705921
Guangyuan <254202042@qq.com>
2023-06-03 16:15:46 +0800

    feat:添加手机号码唯一性校验

--- middleware/validator/userValidator.js
+++ middleware/validator/userValidator.js
@@ -5,14 +5,24 @@ const register = validate([
     body('username').notEmpty().withMessage('用户名不能为空').isLength({min: 3}).withMessage('用户名长度不能小于6位'),
     body('password').notEmpty().withMessage('密码不能为空').isLength({min: 3}).withMessage('密码长度不能小于6位'),
     body('email')
-        .notEmpty().withMessage('邮箱不能为空')
+        .notEmpty().withMessage('邮箱不能为空').bail()
         .isEmail().withMessage('邮箱格式不正确').bail()
         .custom(async email => {
             const user = await User.findOne({email})
             if(user){
                 return Promise.reject('邮箱已经存在')
             }
-        }),
+        }).bail(),
+    
+    body('phone')
+        .notEmpty().withMessage('手机号不能为空').bail()
+        .isMobilePhone('zh-CN').withMessage('手机号格式不正确').bail()
+        .custom(async phone => {
+            const user = await User.findOne({phone})
+            if(user){
+                return Promise.reject('手机号已经存在')
+            }
+        }).bail()
 ])
 
 module.exports = {
