0e3bb304e2f3bae202984515d8d0daa8d27a42d2
Guangyuan <254202042@qq.com>
2023-06-03 16:12:52 +0800

    feat:自定义校验--查询数据库校验

--- middleware/validator/userValidator.js
+++ middleware/validator/userValidator.js
@@ -1,10 +1,18 @@
 const {body} = require('express-validator')
 const validate = require('./errorBack')
-
+const User = require('../../modules/users/usersModel.js')
 const register = validate([
-    body('username').notEmpty().withMessage('用户名不能为空').isLength({min: 6}).withMessage('用户名长度不能小于6位'),
-    body('password').notEmpty().withMessage('密码不能为空').isLength({min: 6}).withMessage('密码长度不能小于6位'),
-    body('email').notEmpty().withMessage('邮箱不能为空').isEmail().withMessage('邮箱格式不正确'),
+    body('username').notEmpty().withMessage('用户名不能为空').isLength({min: 3}).withMessage('用户名长度不能小于6位'),
+    body('password').notEmpty().withMessage('密码不能为空').isLength({min: 3}).withMessage('密码长度不能小于6位'),
+    body('email')
+        .notEmpty().withMessage('邮箱不能为空')
+        .isEmail().withMessage('邮箱格式不正确').bail()
+        .custom(async email => {
+            const user = await User.findOne({email})
+            if(user){
+                return Promise.reject('邮箱已经存在')
+            }
+        }),
 ])
 
 module.exports = {
