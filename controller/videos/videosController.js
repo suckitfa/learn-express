deeb1223f2d9155cec972c92d4fe594d234f67e9
Guangyuan <254202042@qq.com>
2023-06-07 16:22:54 +0800

    feat:新增视频信息接口

--- controller/videos/videosController.js
+++ controller/videos/videosController.js
@@ -1,4 +1,4 @@
-
+const { Video } = require('../../model')
 const list = async (req,res) => {
     res.json({message: 'list',data:[{id:1,name:'video1'},{id:2,name:'video2'}]})
 }
@@ -10,11 +10,20 @@ const getvod = async (req,res) => {
 }
 
 const createvideo = async (req,res) => {
-    const videoInfo = req.body
+    const newVideo = new Video({
+        ...req.body,
+        user: req.user._id
+    })
+    await newVideo.save().catch(err => {
+        res.status(500).json({
+            message: 'createvideo error',
+            error: err
+        })
+    })
     res.json({
         message: 'createvideo',
         video: {
-            ...videoInfo
+            ...req.body
         }
     })
 }
